bfc25711cc8179621f3e2a9b4ae76c9025113759
Barma Vishwanth <vishwanthbsavp@gmail.com>
2021-06-19 19:44:47 +0530

    added branch class

--- Main/main.py
+++ Main/main.py
@@ -17,14 +17,62 @@ class Customer:
         return self.transactions
 
 
-c1 = Customer("Vishwanth", 20)
-print(c1.get_transactions())
-print(c1.get_name())
-c1.add_transaction(30)
-print(c1.get_transactions())
-c2 = Customer("Hima", 340)
-c2.add_transaction(47)
-print(c2.get_transactions())
-print(c2.get_name())
-print(c1.get_name())
+class Branch:
+    def __init__(self, name):
+        self.name = name
+        self.customers = []
+
+    def get_name(self):
+        return self.name
+
+    def get_customers(self):
+        return self.customers
+
+    def find_customer(self, name):
+        for customer in self.customers:
+            if customer.name == name:
+                return customer   # Return the customer if the customer exits
+        return None
+
+    def new_customer(self, name, transaction):
+        if self.find_customer(name) is None:
+            new_customer = Customer(name, transaction)
+            self.customers.append(new_customer)
+            return True   # In case if the customer added to the branch customers list successfully
+        print("Customer '{}' already exists".format(name))
+        return False
+
+    def list_customers(self):
+        if len(self.customers) != 0:
+            print("Bank Customers of Branch '{}' are : ".format(self.name))
+            for index, customer in enumerate(self.customers):
+                print(index+1, ": ", end="")
+                print(customer.name)
+
+    def add_customer_transaction(self, name, transaction):
+        customer = self.find_customer(name)
+        if customer is not None:
+            customer.add_transaction(transaction)
+            return True
+        return False
+
+
+# branch1 = Branch("SBI")
+# branch1.new_customer("Vishwanth", 200)
+# branch1.new_customer("Vishwanth", 302)
+# branch1.new_customer("Hima", 23)
+# branch1.list_customers()
+
+
+#
+# c1 = Customer("Vishwanth", 20)
+# print(c1.get_transactions())
+# print(c1.get_name())
+# c1.add_transaction(30)
+# print(c1.get_transactions())
+# c2 = Customer("Hima", 340)
+# c2.add_transaction(47)
+# print(c2.get_transactions())
+# print(c2.get_name())
+# print(c1.get_name())
 

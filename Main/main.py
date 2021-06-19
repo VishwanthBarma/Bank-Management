eec9f9a2e7d4046ca41461c74b487e01c0bb75d4
Barma Vishwanth <vishwanthbsavp@gmail.com>
2021-06-19 21:04:00 +0530

    added bank class

--- Main/main.py
+++ Main/main.py
@@ -1,4 +1,6 @@
-# Bank Application
+# ***************************
+#  ****BANK APPLICATION****
+# ***************************
 # Customer Class (User)
 class Customer:
     def __init__(self, name, transaction):
@@ -17,6 +19,7 @@ class Customer:
         return self.transactions
 
 
+# Branch Class (Bank)
 class Branch:
     def __init__(self, name):
         self.name = name
@@ -26,7 +29,7 @@ class Branch:
         return self.name
 
     def get_customers(self):
-        return self.customers
+        return list(self.customers)
 
     def find_customer(self, name):
         for customer in self.customers:
@@ -46,7 +49,7 @@ class Branch:
         if len(self.customers) != 0:
             print("Bank Customers of Branch '{}' are : ".format(self.name))
             for index, customer in enumerate(self.customers):
-                print(index+1, ": ", end="")
+                print("Customer ", index+1, ": ", end="")
                 print(customer.name)
 
     def add_customer_transaction(self, name, transaction):
@@ -57,10 +60,96 @@ class Branch:
         return False
 
 
+# Bank Class
+class Bank:
+    def __init__(self, name):
+        self.name = name
+        self.branches = []
+
+    def get_name(self):
+        return self.name
+
+    def get_branches(self):
+        return self.branches
+
+    def find_branch(self, name):
+        if len(self.branches) != 0:
+            for branch in self.branches:
+                if branch.name == name:
+                    return branch
+        return None
+
+    def add_branch(self, name):
+        if self.find_branch(name) is None:
+            new_branch = Branch(name)
+            self.branches.append(new_branch)
+            return True
+        return False
+
+    def add_customer(self, branch_name, customer_name, customer_transaction):
+        branch = self.find_branch(branch_name)
+        if branch is not None:
+            return branch.new_customer(customer_name, customer_transaction)
+        return False
+
+    def add_customer_transaction(self, branch_name, customer_name, customer_transaction):
+        branch = self.find_branch(branch_name)
+        if branch is not None:
+            return branch.add_customer_transaction(customer_name, customer_transaction)
+        return False
+
+    def list_branch_customers(self, branch_name, to_print_transactions : bool):
+        branch = self.find_branch(branch_name)
+        if branch is not None:
+            if to_print_transactions:
+                print("Bank Customers of Branch '{}' are : ".format(branch_name))
+                branch_customers = list(branch.customers)
+                for index, customer in enumerate(branch_customers):
+                    print("Customer ", (index+1), " - ", customer.name)
+                    print("-> Transactions : ", end=" ")
+                    for transaction in customer.transactions:
+                        print(transaction, end=", ")
+                    print()
+            else:
+                branch.list_customers()
+            return True
+        return False
+
+
+bank1 = Bank("National Bank")
+bank1.add_branch("something")
+bank1.add_customer("something", "Vishwanth", 200)
+bank1.add_customer("something", "hima", 280)
+bank1.add_customer("something", "rishi", 190)
+bank1.add_customer_transaction("something", "Vishwanth", 789)
+bank1.add_customer_transaction("something", "rishi", 689)
+
+bank1.list_branch_customers("something", True)
+bank1.list_branch_customers("something", False)
+
+# Bank bank = new Bank("National Australia Bank");
+# bank.addBranch("Adelaide");
+# bank.addCustomer("Adelaide","Tim", 50.05);
+# bank.addCustomer("Adelaide", "Mike", 175.34);
+# bank.addCustomer("Adelaide", "Percy", 220.12);
+# bank.addCustomerTransaction("Adelaide", "Tim", 44.22);
+# bank.addCustomerTransaction("Adelaide", "Tim", 12.44);
+# bank.addCustomerTransaction("Adelaide", "Mike", 1.65);
+#
+# bank.listCustomers("Adelaide", true);
+# System.out.println(" ");
+# System.out.println("***********************");
+# bank.listCustomers("Adelaide", false);
+# }
+
+
+
 # branch1 = Branch("SBI")
 # branch1.new_customer("Vishwanth", 200)
 # branch1.new_customer("Vishwanth", 302)
 # branch1.new_customer("Hima", 23)
+# branch1.list_customers()
+# branch1.add_customer_transaction("Vishwanth", 9999)
 # branch1.list_customers()
 
 
